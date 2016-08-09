7187fb531fde2cca00aad89b85d287b321306de4
waitaction <350561378@qq.com>
2016-08-09 16:29:24 +0800

    简化了示例代码

--- Json2Sql.Test/MainWindow.xaml.cs
+++ Json2Sql.Test/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using Json2Sql.ConditionModel;
-using Json2Sql.ModelEx;
 
 namespace Json2Sql.Test
 {
@@ -19,72 +17,34 @@ namespace Json2Sql.Test
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            var btn = sender as Button;
-            if (btn.Name == "buttonInsert")
+            try
             {
-                try
+                var btn = sender as Button;
+                var jsonEntity = new JsonEntityToSql(ConnectionText.Text);
+                switch (btn.Name)
                 {
-                    var jsonEntity = JsonText.Text.ToJsonDictionary();
-                    var sqlString = new SqlString(jsonEntity);
-                    var str = sqlString.GetInsertSqls();
-
-                    SqlText.Text = str;
-                }
-                catch (Exception ex)
-                {
-                    SqlText.Text = ex.ToString();
+                    case "buttonInsert":
+                        SqlText.Text = jsonEntity.Json2InsertSql(JsonText.Text);
+                        break;
+                    case "buttonDelete":
+                        SqlText.Text = jsonEntity.Json2DeleteSql(JsonText.Text);
+                        break;
+                    case "buttonUpdate":
+                        SqlText.Text = jsonEntity.Json2UpdateSql(JsonText.Text);
+                        break;
+                    case "buttonSelect":
+                        //server=127.0.0.1;user id=root;password=123456abc;persistsecurityinfo=True;database=one_note
+                        SqlText.Text = jsonEntity.Json2SelectSql(JsonText.Text);
+                        break;
+                    default:
+                        break;
                 }
             }
-
-            if (btn.Name == "buttonDelete")
+            catch (Exception ex)
             {
-                try
-                {
-                    var jsonEntity = JsonText.Text.ToJsonDictionary();
-                    var sqlString = new SqlString(jsonEntity);
-                    var str = sqlString.GetDeleteSqls();
-
-                    SqlText.Text = str;
-                }
-                catch (Exception ex)
-                {
-                    SqlText.Text = ex.ToString();
-                }
+                SqlText.Text = ex.ToString();
             }
 
-            if (btn.Name == "buttonUpdate")
-            {
-                try
-                {
-                    var jsonEntity = JsonText.Text.ToJsonDictionary();
-                    var sqlString = new SqlString(jsonEntity);
-                    var str = sqlString.GetUpdateSqls();
-
-                    SqlText.Text = str;
-                }
-                catch (Exception ex)
-                {
-                    SqlText.Text = ex.ToString();
-                }
-            }
-
-            if (btn.Name == "buttonSelect")
-            {
-                try
-                {
-                    var jsonEntity = JsonText.Text.ToJsonObject();
-                    var sqlString = new SqlString(jsonEntity);
-                    //  sqlString.MySqlConnection = new MySql.Data.MySqlClient.MySqlConnection("server=127.0.0.1;user id=root;password=123456abc;persistsecurityinfo=True;database=one_note");
-                    sqlString.MySqlConnection = new MySql.Data.MySqlClient.MySqlConnection(ConnectionText.Text);
-                    var str = sqlString.GetQuerySql();
-                    SqlText.Text = str;
-                }
-                catch (Exception ex)
-                {
-                    SqlText.Text = ex.ToString();
-                }
-
-            }
 
         }
     }
